34c97db57d28aba4c9379e356e236acf86380f8b
Shreya Menon <shreya_menon@college.harvard.edu>
2018-04-06 16:13:42 -0400

    added to story

--- storytime.py
+++ storytime.py
@@ -3,4 +3,6 @@ Use this space to collaboratively write a short story with your team.
 After every few sentences, push your changes to the remote repository and resolve conflicts.
 
 YOUR STORY HERE
+Hi! There once was a penguin named Bob. The penguin had a friend named Sam. 
+They went to school together. 
 '''
