8d2c7c1c8a792f57b94f4aefb0db64b4997b997b
Santiago Vargas <santiagovargas921@gmail.com>
2018-04-06 16:12:49 -0400

    Fresh prince

--- storytime.py
+++ storytime.py
@@ -3,4 +3,8 @@ Use this space to collaboratively write a short story with your team.
 After every few sentences, push your changes to the remote repository and resolve conflicts.
 
 YOUR STORY HERE
+
+
+Now, this is a story all about how
+My life got flipped-turned upside down
 '''
