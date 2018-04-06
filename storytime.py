51e041ba00ba4b191a6c16b2c317696389432e7f
Tim Menke <tim_menke@g.harvard.edu>
2018-04-06 02:19:55 -0400

    storytime file created

---
+++ storytime.py
@@ -0,0 +1,6 @@
+'''
+Use this space to collaboratively write a short story with your team.
+After every few sentences, push your changes to the remote repository and resolve conflicts.
+
+YOUR STORY HERE
+'''
