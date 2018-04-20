6bbc8fac0b6c8f0c657a31e79d6c29f267bc29b3
Shreya Menon <shreya_menon@college.harvard.edu>
2018-04-20 14:47:02 -0400

    time

--- csvSave.py
+++ csvSave.py
@@ -2,7 +2,7 @@ import csv as csv
 	
 def save(rxnID,file,mean,variance):
 	#with open('summary_%s.csv' % rxnID, 'a+') as csvfile:
-	with open('/Users/shreyamenon/Dropbox/%s/summary_%s.csv' % rxnID,'a+') as csvfile:
+	with open('/Users/shreyamenon/Dropbox/%s/summary_%s.csv' % (rxnID,rxnID),'a+') as csvfile:
 		swriter = csv.writer(csvfile)
 		swriter.writerow([file, mean[0],mean[1],mean[2], variance[0],variance[1],variance[2]])
 
