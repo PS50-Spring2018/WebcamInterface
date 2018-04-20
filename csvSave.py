b664c7ddd663beeb65d9a9968dd91b02f69eb66b
Shreya Menon <shreya_menon@college.harvard.edu>
2018-04-20 13:10:14 -0400

    shreya

--- csvSave.py
+++ csvSave.py
@@ -6,7 +6,7 @@ import ShapeDetector as sd
 import csv as csv
 	
 def save(file,mean,variance): 
-	with open('summary.csv','w+') as csvfile:
+	with open('summary.csv','a+') as csvfile:
 		swriter = csv.writer(csvfile)
 		swriter.writerow([file, mean, variance])
 
