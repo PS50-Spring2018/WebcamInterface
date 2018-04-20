568bdf4352642da1434e9947a1ad9f5db98385e9
Santiago Vargas <santiagovargas921@gmail.com>
2018-04-20 16:22:30 -0400

    [temp]

---
+++ display_image.py
@@ -0,0 +1,20 @@
+import time
+import cv2
+import numpy as np
+import CameraOps as co
+import ShapeDetector as sd
+import csvSave as csvSave
+import os
+import datetime
+
+
+if __name__=='__main__':
+	
+	st=''
+	img=np.load()
+	cv2.namedWindow("Display")
+	cv2.imshow("Display",img)
+	cv2.waitKey(0)
+
+
+
