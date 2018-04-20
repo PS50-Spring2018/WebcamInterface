893b7474facf0d90a9b503c7625f5b5d3c5fb99d
Santiago Vargas <santiagovargas921@gmail.com>
2018-04-20 16:00:03 -0400

    [clean]

--- Processor.py
+++ Processor.py
@@ -8,11 +8,11 @@ import os
 import datetime
 
 class Processor:
+	
 	def __init__(self, time, interv,rxn_id):
-		self.reaction_id=rxn_id
-
-		self.t=time
 		
+		self.reaction_id=rxn_id
+	
 		self.interv=interv
 		
 		self.count=0
@@ -20,20 +20,23 @@ class Processor:
 	def run(self):
 		
 		if not os.path.exists("/Users/shreyamenon/Dropbox/%s" % self.reaction_id):
+		
 			os.makedirs("/Users/shreyamenon/Dropbox/%s" % self.reaction_id)
 
 		for i in range(int(self.t/self.interv)):
 			#runs a single image process
 			tempM,tempV=self.iteration()
-			#waits for the determined intercal
-			#time.sleep here
-			#cv2.waitKey(self.interv)
-			#prints output,supress post testing
-			print(tempM,tempV)
+			
+			time.sleep(self.interv) 
+			
+			#print(tempM,tempV)
 			
 	def getTime(self):
+		
 		currentDT = datetime.datetime.now()
+		
 		time=currentDT.strftime('%Y%m%d%H%M%s')
+		
 		return time
 	
 	def iteration(self):
@@ -54,7 +57,7 @@ class Processor:
 		#cv2.namedWindow("Display")
 		
 		center, radius=sd.detect(sd, img)
-		print(center,radius)
+	
 		#cv2.imshow("Display",cv2.circle(img,center,radius,(0,255,0),2))
 		#cv2.waitKey(0)
 	
@@ -67,6 +70,7 @@ class Processor:
 
 		for i in range(int(radius)):
 			delta=int(np.sqrt(int(radius)**2-int(i)**2))
+			
 			left=delta
 			right=delta
 			up=i
@@ -86,6 +90,7 @@ class Processor:
 				up=-center[1]
 
 			delta=int(np.sqrt(int(radius)**2-int(i)**2))
+			
 			x=np.arange(int(center[0])-left,int(center[0])+right-1)
 
 			mask[x,(center[1]+up-1),:]=1
@@ -94,64 +99,15 @@ class Processor:
 		
 
 		zeros=np.multiply(img,mask)
+
 		masked = np.ma.masked_equal(zeros, 0)
 
 		mean=[np.mean(masked[:,:,0]),np.mean(masked[:,:,1]),np.mean(masked[:,:,2])]
+		
 		var=[np.var(masked[:,:,0]),np.var(masked[:,:,1]),np.var(masked[:,:,2])]
 
 		csvSave.save(self.reaction_id,name,mean,var)
 
 		return mean,var
 
-		self.count+=1
-
 		
-		"""
-		r1,r2,r3,r4,r5=np.array([]),np.array([]),np.array([]),np.array([]),np.array([])
-		b1,b2,b3,b4,b5=np.array([]),np.array([]),np.array([]),np.array([]),np.array([])
-		g1,g2,g3,g4,g5=np.array([]),np.array([]),np.array([]),np.array([]),np.array([])
-		
-		#create a mask with 0/1 as being the points iin the  circle or outside them 
-		#multiply the 
-
-		for i in range(int(img.shape[0]/5)):
-									
-									print("*",end="")
-									for j in range(img.shape[1]):		
-										#go back and make this generalizable
-										#these were made into appending blocks to improve performance 
-										#i may try to clock runs later to find a better blocking number but first i need to generalizez it
-										#also indexing needs to be of just the circle, not the entire image
-										if sd.circledetect(5*i,j,center,radius):
-											
-											r1=np.append(r1,img[5*i][j][0])
-											b1=np.append(b1,img[5*i][j][1])
-											g1=np.append(g1,img[5*i][j][2])
-						
-										if sd.circledetect(5*i+1,j,center,radius):
-											
-											r2=np.append(r2,img[5*i+1][j][0])
-											b2=np.append(b2,img[5*i+1][j][1])
-											g2=np.append(g2,img[5*i+1][j][2])
-										
-										if sd.circledetect(5*i+2,j,center,radius):
-											
-											r3=np.append(r3,img[5*i+2][j][0])
-											b3=np.append(b3,img[5*i+2][j][1])
-											g3=np.append(g3,img[5*i+2][j][2])
-										
-										if sd.circledetect(5*i+3,j,center,radius):
-											
-											r4=np.append(r4,img[5*i+3][j][0])
-											b4=np.append(b4,img[5*i+3][j][1])
-											g4=np.append(g4,img[5*i+3][j][2])
-										
-										if sd.circledetect(5*i+4,j,center,radius):
-						
-											r5=np.append(r5,img[5*i+4][j][0])
-											b5=np.append(b5,img[5*i+4][j][1])
-											g5=np.append(g5,img[5*i+4][j][2])			"""
-				
-		"""r=np.append(np.append(np.append(r1,r2),np.append(r3,r4)),r5)
-			b=np.append(np.append(np.append(b1,b2),np.append(b3,b4)),b5)
-			g=np.append(np.append(np.append(g1,g2),np.append(g3,g4)),g5)"""
--- ShapeDetector.py
+++ ShapeDetector.py
@@ -3,34 +3,19 @@ import numpy as np
 	
 def detect(self, initial_img):
 	# initialize the shape name and approximate the contour
-	#contours,hierarchy = cv2.findContours(c, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	#print(img)
-
-	#derived from https://www.pyimagesearch.com/2016/02/08/opencv-shape-detection/
-
+	
 	gray = cv2.cvtColor(initial_img, cv2.COLOR_BGR2GRAY)
 	#print(gray)
 	blur = cv2.GaussianBlur(gray, (5, 5), 0)
 	#print(blur)
 	thresh = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY_INV)[1]
-	#ask tim about the allowing thresh to pass
 
-	#cv2.namedWindow("test")
-
-	cv2.imshow("test",thresh)
-	
-	#find contours isn't working
 	_, cont,hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-	print(len(cont))
-	cv2.imshow("test",cv2.drawContours(thresh.copy(), cont, -1, (0,255,0), 8))
-	cv2.waitKey(0)
-
+	
 	center=[]
 	radii=[]
 
 	for line in cont:
-
-		#line = cv2.approxPolyDP(line, 0.01 * cv2.arcLength(line, True), True)
 
 		temp=circle(line)
 		
@@ -42,20 +27,9 @@ def detect(self, initial_img):
 	
 	ind=np.argmax(radii)
 
-	#for i in range(len(radii)):
-
-		#print(center[i],radii[i])
-
-	#for i in range(len(center)):
-
-		#cv2.imshow("test",cv2.circle(initial_img,center[i],radii[i],(0,255,0),2))
-
-		#cv2.waitKey(0)
-	#print(radii[ind])
 	return center[ind],radii[ind]
 
-def circle(cnt): 
-    
+def circle(cnt):   
 
 	(x,y),radius = cv2.minEnclosingCircle(cnt)
 
@@ -64,16 +38,3 @@ def circle(cnt):
 	radius = int(radius)
 
 	return center, radius 
-
-
-def circledetect(x,y,center,radius):
-
-	vect=np.array([(x-center[0]),(y-center[0])])
-	
-	if np.linalg.norm(vect)<radius: 
-	
-		return True
-	
-	else:
-	
-		return False 
